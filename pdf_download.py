d845895489c6d0962530902a14883504fde30b16
RamazanDemirci <radem18@gmail.com>
2020-12-27 21:07:44 +0300

    code review

--- pdf_download.py
+++ pdf_download.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import requests
-from fpdf import FPDF
 import glob
 
 #imagelist = []
@@ -10,7 +9,7 @@ cover_url = "http://www.ataekitap.com/e-kitaplar/ortaokul/5-sinif/5_Sinif_Ben_Ko
 
 img_cover = Image.open(requests.get(cover_url, stream = True).raw)
 
-for i in range(175,185):
+for i in range(2,306):
     print(f"process in {i}/305")
     image_url = f"http://www.ataekitap.com/e-kitaplar/ortaokul/5-sinif/5_Sinif_Ben_Korkmam_Fen_Bilimleri_Soru_Bankasi/files/mobile/{i}.jpg?200725164621"
     try:
